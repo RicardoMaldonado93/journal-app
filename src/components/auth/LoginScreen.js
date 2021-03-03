04ea4b2ef9673dd7be83d0b9900ea129b0693a32
Ricardo Maldonado <rimaldonado@servexternos.santandertecnologia.com.ar>
2021-03-03 01:19:17 -0300

    add LoginScreen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const LoginScreen = () => {
     return (
         <div>
-            <h2>LoginScreen</h2>
+            <h2>LoginScreen != </h2>
         </div>
     )
 }
